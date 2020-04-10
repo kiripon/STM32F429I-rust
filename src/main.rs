2a4eb501321864e4e5e56960c4774e530602a531
Kiripon <painapoo@gmail.com>
2020-04-10 17:01:10 +0900

    Lチカに成功

--- src/main.rs
+++ src/main.rs
@@ -7,32 +7,32 @@ extern crate panic_halt;
 // extern crate panic_itm; // logs messages over ITM; requires ITM support
 // extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 extern crate stm32f4;
+use crate::hal::{prelude::*, stm32};
 use cortex_m::asm;
 use cortex_m_rt::entry;
-use cortex_m_semihosting::hprintln;
-
+use hal::delay;
 use stm32f4xx_hal as hal;
 
-use crate::hal::{prelude::*, stm32};
 
 #[entry]
 fn main() -> ! {
     // asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
     // hprintln!("hello world!").unwrap();
 
-    if let (Some(dp), Some(_cp)) = (
+    if let (Some(dp), Some(cp)) = (
         stm32::Peripherals::take(),
         cortex_m::peripheral::Peripherals::take(),
     ) {
         let gpiog = dp.GPIOG.split();
         let mut led = gpiog.pg13.into_push_pull_output();
-        
+        let rcc = dp.RCC.constrain();
+        let clocks = rcc.cfgr.sysclk(180.mhz()).freeze();
+
+        let mut delay = delay::Delay::new(cp.SYST, clocks);
         for i in 1.. {
-            hprintln!("i: {}", i).unwrap();
             led.toggle().unwrap();
         }
     }
-    
     loop {
         asm::wfe();
     }
